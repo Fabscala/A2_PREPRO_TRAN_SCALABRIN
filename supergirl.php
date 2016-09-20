89a6fd973e9925606b44d264e3f1f819bfbb4f5d
Kevin Tran <kevintran@hotmail.de>
2016-09-20 22:25:45 +0200

    supergirl page bug fixed

--- supergirl.php
+++ supergirl.php
@@ -22,7 +22,7 @@
 
 
                      <div id="landing-link" class="supergirl-link-width">
-                            <a href="#arrow">
+                            <a href="#supergirl-synopsis">
                                    <p id="landing-link-text">decouvrir supergirl<br> <i class="fa fa-angle-down" aria-hidden="true"></i></p>
                             </a>
                      </div>
@@ -55,13 +55,13 @@
               <div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/alexandra.png) no-repeat 0px 0px;">
                      </div>
-                     <div class="character-name">Alexandra « Alex » Danvers</div>
+                     <div class="character-name">Alexandra Danvers</div>
               </div>
 
               <div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/winn.png) no-repeat 0px 0px;">
                      </div>
-                     <div class="character-name">Winslow « Winn » Schott</div>
+                     <div class="character-name">Winslow Schott</div>
               </div>
 
               <div id="characters">
