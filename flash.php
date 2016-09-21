ae62e6de359005e5da2c36057853c80421854a7c
Fabien Scalabrin <fscalabrin2@gmail.com>
2016-09-21 16:35:34 +0200

    description add on flash supergirl and lot

--- flash.php
+++ flash.php
@@ -43,59 +43,68 @@
               <div id="flash-characters">
               <h2>personnages</h2>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/barry.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Barry Allen</div>
               </div>
+              <span>Bartholomew Henry "Barry" Allen (né en 1989), est un scientifique travaillant en tant que médecin légiste au CCPD. Barry est le fils d'Henry Allen et de Nora Allen, le fils adoptif de Joe West et le meilleur ami/frère adoptif d'Iris West.</span></a>
        
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/iris.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Iris West</div>
               </div>
+              <span>Iris West est la fille de Joe West et l'ami d'enfance de Barry Allen. Elle est journaliste et elle est fascinée par The Flash. Elle est également la petite amie du collègue de son père Eddie Thawne pendant la Saison 1 (The Flash).</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/cait.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Caitlin Snow</div>
               </div>
+              <span>Caitlin Snow est une ingénieur en biologie à S.T.A.R. Labs. Elle reste aux côtés d'Harrison Wells"" après la fermeture de l'entreprise.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/cisco.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Francisco Ramon</div>
               </div>
+              <span>Cisco Ramon est un ingénieur en mécanique à S.T.A.R. Labs. Il reste aux côtés d'Harrison Wells/Eobard Thawne après la fermeture de l'entreprise.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/harry.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Harrison Wells</div>
               </div>
+              <span>Harrison Wells était un scientifique de renom et prometteur, marié à Tess Morgan et un bon ami de Tina McGee. Il fut tué par le méta-humain Eobard Thawne venu du futur, qui usurpa son identité pendant près de deux décennies.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/joe.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Joe West</div>
               </div>
+              <span>Joe West est un détective de police de Iris West. C'est également le père adoptif de Barry Allen.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/wally.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Wally West</div>
               </div>
+              <span>Wally West est le fils de Joe et Francine West ainsi que le frère cadet d'Iris West.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/eddy.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Eddie Thawne</div>
               </div>
+              <span>Eddie Thawne est un détective de la police de Central City et le collègue de Joe West. Il sort avec Iris West.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_flash/henry.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Henry Allen</div>
               </div>
+              <span>Henry Allen était le père de Barry Allen, le mari de Nora Allen et un très bon ami de Joe West. Après que sa femme ait été assassinée par le voyageur temporel Reverse-Flash, il fut accusé du meurtre et envoyé en prison à Iron Heights.</span></a>
                      
 
        </div>
--- supergirl.php
+++ supergirl.php
@@ -41,41 +41,47 @@
        <div id="supergirl-characters">
               <h2>personnages</h2>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/supergirl.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Kara Danvers</div>
               </div>
+              <span>Kara Zor-El / Kara Danvers / Supergirl est une kryptonnienne envoyée sur Terre pour protégée son cousin. Mais elle fut bloquée dans la Zone Fantôme et son cousin est devenu Clark Kent alias Superman.</span></a>
        
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/jimmy.png) no-repeat 0px 0px;">
                      </div>
-                     <div class="character-name">Jimmy Olsen</div>
+                     <div class="character-name">James Olsen</div>
               </div>
+              <span>James Olsen est un photographe pour CatCo.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/alexandra.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Alexandra Danvers</div>
               </div>
+              <span>Alexandra "Alex" Danvers est employée d'une organisation secrète du gouvernement et la sœur adoptive de Kara Danvers.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/winn.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Winslow Schott</div>
               </div>
+              <span>Winslow Schott est un programmeur et travaille avec Kara Danvers à CatCo.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/hank.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Hank Henshaw</div>
               </div>
+              <span>Hank Henshaw était le Directeur de la D.E.O., une organisation secrète qui recherche des êtres venus de la bordure extérieur. Lors d'une mission avec le Dr. Jeremiah Danvers, il fut tué.</span></a>
 
-              <div id="characters">
+              <a href=""><div id="characters">
                      <div class="rondperso" style="background:url(img/img_supergirl/cat.png) no-repeat 0px 0px;">
                      </div>
                      <div class="character-name">Cat Grant</div>
               </div>
+              <span>Cat Grant est la fondatrice de CatCo. Son assistante est Kara Danvers.</span></a>
 
 
 
